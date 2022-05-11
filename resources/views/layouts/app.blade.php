5bbb73d7edde5e7c4756f66a311e58be03caed1f
Bisnu kundu <bisnukundu@gmail.com>
2022-05-12 02:23:10 +0600

    working with markup and setup dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html :class="{ 'dark': dark }" x-data="data()" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <!-- Scripts -->
+    <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <script src="{{ asset('js/app.js') }}" defer></script>
-    @livewireStyles
+    <script src="{{ asset('js/init-alpine.js') }}"></script>
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+
+    </style>
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
-
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+<body>
+    <div x-cloak class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+        {{-- Desktop sidebar --}}
+        @include('layout_files.desktop_sidebar')
+        {{-- Mobile sidebar --}}
+        @include('layout_files.mobile_sidebar')
+        <div class="flex flex-col flex-1 w-full">
+            {{-- Header --}}
+            @include('layout_files.header')
+            <main class="h-full overflow-y-auto">
+                <div class="container px-6 mx-auto grid">
+                    {{$slot}}
+                </div>
+            </main>
+        </div>
     </div>
     @livewireScripts
     <x-livewire-alert::scripts />
